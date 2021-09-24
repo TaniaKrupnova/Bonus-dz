48fda303ffa8ae1e809fe73987901eeccde4c335
TaniaKrupnova <88766616+TaniaKrupnova@users.noreply.github.com>
2021-09-24 15:43:29 +0300

    Update Main.java

--- src/Main.java
+++ src/Main.java
@@ -5,36 +5,17 @@ public class Main {
         int onAccount = 100;
         int sumTicket = 5_000;
 
-
-        if (sumTicket >= 1_000) {
-            int bonus = (sumTicket / 100) + onAccount +sumTicket;
-            System.out.println("Всего с бонусами:" + bonus);
+        int bonus;
+        if (sumTicket <= 1_000) {
+            bonus = 0;
+            
         }else {
-            int bonus = 0;
-            int total = onAccount + sumTicket;
-            System.out.println("Всего на счете:" + total);
+            bonus = sumTicket / 100;
+            
         }
-
+         int total = onAccount + sumTicket + bonus;
+            System.out.println("Всего на счете:" + total);
+            System.out.println("Всего бонусов:" + bonus);
     }
 
 }
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
